624ffa99c66bae7b5020eed91e283b875ad56888
chewitt1 <carrimahewitt3@gmail.com>
2023-09-17 19:21:35 -0400

    Add Drawer

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+/*BRAND*/
+let brand = document.querySelectorAll(".brand");
+
+brand.forEach((brand) => {
+    brand.addEventListener("click", ()=>{
+        if(brand.classList.contains("page-brand")){
+            document.location = "../index.html";
+        }
+        else{
+            document.location = "./index.html";
+        }
+    })
+});
+/*DRAWER*/
+let drawer = document.querySelector("#drawer");
+let menu = document.querySelector("#menu");
+let drawerOpen = false;
+
+function toggleDrawer(){
+    if(drawerOpen){
+        drawer.style.display = "none";;
+        drawerOpen = false;
+    }
+    else{
+        drawer.style.display = "flex";
+        drawerOpen = true;
+    }
+}
+
+menu.addEventListener("click", ()=>{toggleDrawer();})
+/*SLIDES*/
 let credit = document.querySelector("#credit");
 let slides = document.querySelector("#slides");
 let forwardBtn = document.querySelector("#fwd-btn");
